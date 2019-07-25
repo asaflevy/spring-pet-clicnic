88c5073fe573322dba7cea4947348e3d543ebdb6
asafart <asaf@artlist.io>
2019-07-25 14:20:34 +0300

    first commit

---
+++ src/main/java/com/spring/springpetclicnic/SpringPetClicnicApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springpetclicnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClicnicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClicnicApplication.class, args);
+    }
+
+}
